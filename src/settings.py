91a8da8612e92df47214f0ea4c34e92ab4ceeb8d
Saniya Maheshwari <saniya.mah@gmail.com>
2021-11-05 20:14:36 +0530

    Reset settings file for upload

--- src/settings.py
+++ src/settings.py
@@ -1,24 +1,39 @@
 """Global settings for the project.
 
-This is automatically modified by setup.sh. Otherwise for a custom setup you
-need to modify this file yourself.
+Entries that have value 'None' are automatically filled in by setup.sh.
 """
 
 # BERT and co.
-BERT_ROOT = '/home/saniya/xcs224u/project/models/bert/'
-BERTWEET_ROOT = '/home/saniya/xcs224u/project/models/bertweet/'
-HINDIBERT_ROOT = '/home/saniya/xcs224u/project/models/hindi-bert/'
-INDICBERT_ROOT = '/home/saniya/xcs224u/project/models/indic-bert/'
+BERT_ROOT = None
+BERTWEET_ROOT = None
+HINDIBERT_ROOT = None
+INDICBERT_ROOT = None
 
 # word embeddings
-GLOVE_ROOT = '/home/saniya/xcs224u/project/models/glove/'
-FASTTEXT_ROOT = '/home/saniya/xcs224u/project/models/fasttext/'
-INDICFT_ROOT = '/home/saniya/xcs224u/project/models/indicft/'
+FASTTEXT_ROOT = None
+INDICFT_ROOT = None
 
 # datasets
-EN_TWEETS_ROOT = '/home/saniya/xcs224u/project/data/semeval-2017-4a/'
-HI_EN_TWEETS_ROOT = '/home/saniya/xcs224u/project/data/semeval-2020-9/'
+ENG_TWEETS_ROOT = None
+HIN_ENG_TWEETS_ROOT = None
 
 # canonical corpora
-EN_CORPUS_ROOT = '/home/saniya/xcs224u/project/data/statmt-1bw/'
-HI_CORPUS_ROOT = '/home/saniya/xcs224u/project/data/indiccorp-hi/'
+ENG_CORPUS_ROOT = None
+HIN_CORPUS_ROOT = None
+
+# challenge test sets
+CHALLENGE_ROOT = None
+
+TESTSET_FILENAMES = {
+    'mono-eng': None,
+    'mono-eng-challenge': 'mono_eng_challenge.txt',
+    'mono-eng-easy': 'mono_eng_easy.txt',
+    'codemix': None,
+    'codemix-eng': 'codemix_eng.txt',
+    'codemix-mixed':  'codemix_mixed.txt',
+    'codemix-hin': 'codemix_hin.txt',
+    'codemix-hin-challenge': 'codemix_hin_challenge.txt',
+    'codemix-hin-easy': 'codemix_hin_easy.txt'
+}
+
+PERPLEXITIES_FILE = 'perplexities.txt'
